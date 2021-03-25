66608fa38fc0c398a908385c06db0588ece31f44
Tom de Geus <tom@geus.me>
2021-03-25 16:57:49 +0100

    No xfixed on Windows

--- example/example.cpp
+++ example/example.cpp
@@ -1,6 +1,8 @@
 #include <xtensor/xarray.hpp>
 #include <xtensor/xtensor.hpp>
-#include <xtensor/xfixed.hpp>
+#ifndef _WIN32
+#include <xtensor/xfixed.hpp> // has issues in Windows
+#endif
 #include <pyxtensor/pyxtensor.hpp>
 
 xt::xarray<double> arrayTimesTwo(const xt::xarray<double>& a)
@@ -13,10 +15,12 @@ xt::xtensor<double, 2> tensorTimesTwo(const xt::xtensor<double, 2>& a)
     return 2.0 * a;
 }
 
+#ifndef _WIN32
 xt::xtensor_fixed<double, xt::xshape<5, 5>> fixedTimesTwo(const xt::xtensor_fixed<double, xt::xshape<5, 5>>& a)
 {
     return 2.0 * a;
 }
+#endif
 
 namespace py = pybind11;
 
@@ -24,5 +28,7 @@ PYBIND11_MODULE(example, m)
 {
     m.def("arrayTimesTwo", &arrayTimesTwo);
     m.def("tensorTimesTwo", &tensorTimesTwo);
+#ifndef _WIN32
     m.def("fixedTimesTwo", &fixedTimesTwo);
+#endif
 }
--- example/main.py
+++ example/main.py
@@ -1,8 +1,11 @@
 import example
 import numpy as np
+import sys
 
 d = np.arange(25).reshape(5, 5)
 
 assert np.allclose(2.0 * d, example.arrayTimesTwo(d))
 assert np.allclose(2.0 * d, example.tensorTimesTwo(d))
-assert np.allclose(2.0 * d, example.fixedTimesTwo(d))
+
+if sys.platform != 'win32':
+    assert np.allclose(2.0 * d, example.fixedTimesTwo(d))
