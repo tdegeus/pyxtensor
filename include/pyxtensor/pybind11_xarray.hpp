8f78272b4fb9920f09bd4d72549396a03ed4c5f3
Tom de Geus <tom@geus.me>
2021-03-25 17:45:15 +0100

    Style update

--- include/pyxtensor/pybind11_xarray.hpp
+++ include/pyxtensor/pybind11_xarray.hpp
@@ -22,64 +22,64 @@ template<typename T> struct type_caster<xt::xarray<T>>
 {
 public:
 
-  using tensor = xt::xarray<T>;
+    using tensor = xt::xarray<T>;
 
-  PYBIND11_TYPE_CASTER(tensor, _("xt::xarray<T>"));
+    PYBIND11_TYPE_CASTER(tensor, _("numpy.ndarray[") + npy_format_descriptor<T>::name + _("]"));
 
-  // Python -> C++
-  // -------------
+    // Python -> C++
+    // -------------
 
-  bool load(py::handle src, bool convert)
-  {
-    // - basic pybind11 check
-    if ( !convert && !py::array_t<T>::check_(src) ) return false;
+    bool load(py::handle src, bool convert)
+    {
+        // - basic pybind11 check
+        if ( !convert && !py::array_t<T>::check_(src) ) return false;
 
-    // - storage requirements : contiguous and row-major storage from NumPy
-    auto buf = py::array_t<T, py::array::c_style | py::array::forcecast>::ensure(src);
-    // - check
-    if ( !buf ) return false;
+        // - storage requirements : contiguous and row-major storage from NumPy
+        auto buf = py::array_t<T, py::array::c_style | py::array::forcecast>::ensure(src);
+        // - check
+        if ( !buf ) return false;
 
-    // - get dimension of the input array (rank, number of indices)
-    auto N = buf.ndim();
-    // - check
-    if ( N < 1 ) return false;
+        // - get dimension of the input array (rank, number of indices)
+        auto N = buf.ndim();
+        // - check
+        if ( N < 1 ) return false;
 
-    // - shape of the input array
-    std::vector<size_t> shape(N);
-    // - copy
-    for ( ssize_t i = 0 ; i < N ; ++i ) shape[i] = buf.shape()[i];
+        // - shape of the input array
+        std::vector<size_t> shape(N);
+        // - copy
+        for ( ssize_t i = 0 ; i < N ; ++i ) shape[i] = buf.shape()[i];
 
-    // - all checks passed : create the proper C++ variable
-    value.resize(shape);
-    // - copy all data
-    std::copy(buf.data(), buf.data()+buf.size(), value.begin());
+        // - all checks passed : create the proper C++ variable
+        value.resize(shape);
+        // - copy all data
+        std::copy(buf.data(), buf.data()+buf.size(), value.begin());
 
-    // - signal successful variable creation
-    return true;
-  }
+        // - signal successful variable creation
+        return true;
+    }
 
-  // C++ -> Python
-  // -------------
+    // C++ -> Python
+    // -------------
 
-  static py::handle cast(const xt::xarray<T>& src, py::return_value_policy, py::handle)
-  {
-    // - get shape and strides
-    auto xshape   = src.shape();
-    auto xstrides = src.strides();
+    static py::handle cast(const xt::xarray<T>& src, py::return_value_policy, py::handle)
+    {
+        // - get shape and strides
+        auto xshape   = src.shape();
+        auto xstrides = src.strides();
 
-    // - convert to vector
-    std::vector<size_t> shape(xshape.begin(), xshape.end());
-    std::vector<size_t> strides(xstrides.begin(), xstrides.end());
+        // - convert to vector
+        std::vector<size_t> shape(xshape.begin(), xshape.end());
+        std::vector<size_t> strides(xstrides.begin(), xstrides.end());
 
-    // - convert strides to bytes
-    for ( auto &i : strides ) i *= sizeof(T);
+        // - convert strides to bytes
+        for ( auto &i : strides ) i *= sizeof(T);
 
-    // - create Python variable (all variables are copied)
-    py::array a(std::move(shape), std::move(strides), src.begin());
+        // - create Python variable (all variables are copied)
+        py::array a(std::move(shape), std::move(strides), src.begin());
 
-    // - release variable to Python
-    return a.release();
-  }
+        // - release variable to Python
+        return a.release();
+    }
 };
 
 // =================================================================================================
--- include/pyxtensor/pybind11_xtensor_fixed.hpp
+++ include/pyxtensor/pybind11_xtensor_fixed.hpp
@@ -18,73 +18,73 @@ namespace detail {
 // type caster: xt::xtensor_fixed <-> NumPy-array
 // =================================================================================================
 
-template<typename T, typename S> struct type_caster<xt::xtensor_fixed<T,S>>
+template<typename T, typename S> struct type_caster<xt::xtensor_fixed<T, S>>
 {
 public:
 
-  using tensor = xt::xtensor_fixed<T,S>;
+    using tensor = xt::xtensor_fixed<T, S>;
 
-  PYBIND11_TYPE_CASTER(tensor, _("xt::xtensor_fixed<T,S>"));
+    PYBIND11_TYPE_CASTER(tensor, _("numpy.ndarray[") + npy_format_descriptor<T>::name + _("](fixed-shape)"));
 
-  // Python -> C++
-  // -------------
+    // Python -> C++
+    // -------------
 
-  bool load(py::handle src, bool convert)
-  {
-    // - basic pybind11 check
-    if ( !convert && !py::array_t<T>::check_(src) ) return false;
+    bool load(py::handle src, bool convert)
+    {
+        // - basic pybind11 check
+        if ( !convert && !py::array_t<T>::check_(src) ) return false;
 
-    // - storage requirements : contiguous and row-major storage from NumPy
-    auto buf = py::array_t<T, py::array::c_style | py::array::forcecast>::ensure(src);
-    // - check
-    if ( !buf ) return false;
+        // - storage requirements : contiguous and row-major storage from NumPy
+        auto buf = py::array_t<T, py::array::c_style | py::array::forcecast>::ensure(src);
+        // - check
+        if ( !buf ) return false;
 
-    // - dimension of the input array (rank, number of indices)
-    auto dimension = buf.ndim();
-    // - check
-    if ( dimension < 1 ) return false;
+        // - dimension of the input array (rank, number of indices)
+        auto dimension = buf.ndim();
+        // - check
+        if ( dimension < 1 ) return false;
 
-    // - shape of the input array
-    std::vector<size_t> shape(dimension);
-    // - copy
-    for ( ssize_t i = 0 ; i < dimension ; ++i ) shape[i] = buf.shape()[i];
+        // - shape of the input array
+        std::vector<size_t> shape(dimension);
+        // - copy
+        for ( ssize_t i = 0 ; i < dimension ; ++i ) shape[i] = buf.shape()[i];
 
-    // - temporary variable to check of shape: really not elegant, should be improved
-    xt::xtensor_fixed<T,S> tmp = xt::empty<T>(shape);
-    // - check shape
-    for ( size_t i = 0 ; i < shape.size() ; ++i ) if ( tmp.shape()[i] != shape[i] ) return false;
+        // - temporary variable to check of shape: really not elegant, should be improved
+        xt::xtensor_fixed<T, S> tmp = xt::empty<T>(shape);
+        // - check shape
+        for ( size_t i = 0 ; i < shape.size() ; ++i ) if ( tmp.shape()[i] != shape[i] ) return false;
 
-    // - all checks passed : create the proper C++ variable
-    value.resize(shape);
-    // - copy all data
-    std::copy(buf.data(), buf.data()+buf.size(), value.begin());
+        // - all checks passed : create the proper C++ variable
+        value.resize(shape);
+        // - copy all data
+        std::copy(buf.data(), buf.data()+buf.size(), value.begin());
 
-    // - signal successful variable creation
-    return true;
-  }
+        // - signal successful variable creation
+        return true;
+    }
 
-  // C++ -> Python
-  // -------------
+    // C++ -> Python
+    // -------------
 
-  static py::handle cast(const xt::xtensor_fixed<T,S>& src, py::return_value_policy, py::handle)
-  {
-    // - get shape and strides
-    auto xshape   = src.shape();
-    auto xstrides = src.strides();
+    static py::handle cast(const xt::xtensor_fixed<T, S>& src, py::return_value_policy, py::handle)
+    {
+        // - get shape and strides
+        auto xshape   = src.shape();
+        auto xstrides = src.strides();
 
-    // - convert to vector
-    std::vector<size_t> shape(xshape.begin(), xshape.end());
-    std::vector<size_t> strides(xstrides.begin(), xstrides.end());
+        // - convert to vector
+        std::vector<size_t> shape(xshape.begin(), xshape.end());
+        std::vector<size_t> strides(xstrides.begin(), xstrides.end());
 
-    // - convert strides to bytes
-    for ( auto &i : strides ) i *= sizeof(T);
+        // - convert strides to bytes
+        for ( auto &i : strides ) i *= sizeof(T);
 
-    // - create Python variable (all variables are copied)
-    py::array a(std::move(shape), std::move(strides), src.begin());
+        // - create Python variable (all variables are copied)
+        py::array a(std::move(shape), std::move(strides), src.begin());
 
-    // - release variable to Python
-    return a.release();
-  }
+        // - release variable to Python
+        return a.release();
+    }
 };
 
 // =================================================================================================
